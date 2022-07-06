7eb8bb6209555fefabef9a9a652d629885a8c7dd
unknown <tanhailong@Tonghao.local>
2022-07-06 17:24:42 +0800

    try outer profile rebar

---
+++ try/outer-profile-rebar.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    const int RADIUS = 11;
+    const int LAYER = 100;
+
+    int height, width;
+    cout << "height: " << endl;
+    cin >> height;
+    cout << "width: " << endl;
+    cin >> width;
+
+    int a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y, 
+    e_x, e_y, g_x, g_y, h_x, h_y, j_x, j_y;
+    a_x = LAYER + 3 * RADIUS;
+    a_y = LAYER + RADIUS + 300;
+    b_x = a_x;
+    b_y = a_y - 300;
+    c_x = width - a_x;
+    c_y = b_y;
+    d_x = c_x;
+    d_y = a_y;
+    e_x = a_x - 2 * RADIUS;
+    e_y = b_y;
+    g_x = e_x;
+    g_y = height - b_y;
+    h_x = width - e_x;
+    h_y = g_y;
+    j_x = h_x;
+    j_y = e_y;
+
+    cout << "A: ( " << a_x << " , " << a_y << " )" << endl;
+    cout << "B: ( " << b_x << " , " << b_y << " )" << endl;
+    cout << "C: ( " << c_x << " , " << c_y << " )" << endl;
+    cout << "D: ( " << d_x << " , " << d_y << " )" << endl;
+    cout << "E: ( " << e_x << " , " << e_y << " )" << endl;
+    cout << "G: ( " << g_x << " , " << g_y << " )" << endl;
+    cout << "H: ( " << h_x << " , " << h_y << " )" << endl;
+    cout << "J: ( " << j_x << " , " << j_y << " )" << endl;
+
+    system("pause");
+    return 0;
+}
